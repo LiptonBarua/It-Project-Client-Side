8a241f6e2d0818d9f5196b392791458d2807254e
Lipton Barua <liptonbarua274@gmail.com>
2023-05-26 23:01:56 +0600

    project

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -79,7 +79,7 @@ const Header = () => {
 
                 
                 {
-                  navbar ? <HiMenu onClick={() => setDrop(!drop)} className="text-[#C60017] w-8 h-8 text-bold"></HiMenu> : <svg onClick={() => setDrop(!drop)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 text-bold">
+                  navbar ? <HiMenu onClick={() => setDrop(!drop)} className="text-[#C60017] w-8 h-8 text-bold"></HiMenu> : <svg onClick={() => setDrop(!drop)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 text-bold text-[#340110]">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg>
 
