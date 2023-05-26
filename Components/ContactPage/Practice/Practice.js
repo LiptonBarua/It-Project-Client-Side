85cf14ca7dadc8f7d8b2fb52260e238cf9ca334f
Lipton Barua <liptonbarua274@gmail.com>
2023-05-26 12:14:56 +0600

    scan code set

--- Components/ContactPage/Practice/Practice.js
+++ Components/ContactPage/Practice/Practice.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import QRCode from "qrcode.react";
 
 const Practice = () => {
     return (
         <div>
-            
+            <div>
+        <QRCode value="https://www.google.com/" style={{marginRight: 50 }} />
+        <p>google</p>
+    </div>
         </div>
     );
 };
