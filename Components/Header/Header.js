8ae6f15c023b06f1492438b7aed234666d7591ef
Lipton Barua <liptonbarua274@gmail.com>
2023-05-26 22:59:48 +0600

    project

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -25,9 +25,9 @@ const Header = () => {
         <div className="border-b-2 border-gray-400 mb-3 w-full flex items-center p-2 space-x-4">
             <img src={userProfile[0]?.image} alt="" className="w-12 h-12 rounded-full" />
             <div>
-              <h2 className="text-lg font-semibold">{userProfile[0]?.firstName} {userProfile[0]?.lastName}</h2>
+              <h2 className="text-lg font-semibold text-[#340110]">{userProfile[0]?.firstName} {userProfile[0]?.lastName}</h2>
               <span className="flex items-center space-x-1">
-                <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-400">View profile</a>
+                <a rel="noopener noreferrer" href="#" className="text-xs text-[#340110] hover:underline ">View profile</a>
               </span>
             </div>
           </div>
@@ -36,18 +36,18 @@ const Header = () => {
           : ''
       }
   
-      <li onClick={() => setNavbar(!navbar)}><Link href='/' className='relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Home</Link></li>
-      <li onClick={() => setNavbar(!navbar)}><Link href='/About' className='relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>About Us</Link></li>
-      <li onClick={() => setNavbar(!navbar)}><Link href='/Blog' className='relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Blog</Link></li>
-      <li onClick={() => setNavbar(!navbar)}><Link href='/Career' className='relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>My Information</Link></li>
-      <li onClick={() => setNavbar(!navbar)}><Link href='/Contact' className='relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Contact Us</Link></li>
+      <li onClick={() => setNavbar(!navbar)}><Link href='/' className='text-[#340110] font-bold relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Home</Link></li>
+      <li onClick={() => setNavbar(!navbar)}><Link href='/About' className='text-[#340110] font-bold relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>About Us</Link></li>
+      <li onClick={() => setNavbar(!navbar)}><Link href='/Blog' className='text-[#340110] font-bold relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Blog</Link></li>
+      <li onClick={() => setNavbar(!navbar)}><Link href='/Career' className='text-[#340110] font-bold relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>My Information</Link></li>
+      <li onClick={() => setNavbar(!navbar)}><Link href='/Contact' className='text-[#340110] font-bold relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Contact Us</Link></li>
 
       {
-        user?.email ? <li onClick={()=>{handleLogOut(); setNavbar(!navbar)}}><Link href='' className='relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Log Out</Link></li>
+        user?.email ? <li onClick={()=>{handleLogOut(); setNavbar(!navbar)}}><Link href='' className='text-[#340110] font-bold relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Log Out</Link></li>
           :
           <>
             <li onClick={() => setNavbar(!navbar)}><Link href='/Login' className='relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Login</Link></li>
-            <li onClick={() => setNavbar(!navbar)}><Link href='/Register' className='relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Register</Link></li>
+            <li onClick={() => setNavbar(!navbar)}><Link href='/Register' className='text-[#340110] font-bold relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-[#C40017] before:transition hover:before:scale-100' style={{ transitionDuration: `6s` }}>Register</Link></li>
           </>
       }
 
@@ -60,7 +60,7 @@ const Header = () => {
          <Link href='/'>
          <div>
           <h1 className='font-bold text-xl lg:text-3xl text-[#340110]'>NOUR ALKHOR</h1>
-          <p className='text-sm'>Information Technology Co.</p>
+          <p className='text-sm md:text-lg text-[#340110]'>Information Technology Co.</p>
           </div>
          </Link>
 
@@ -69,8 +69,8 @@ const Header = () => {
 
         <div className='flex'>
           <div className='hidden md:block mr-10'>
-            <h1>Call Us</h1>
-            <h1 className='font-semibold'>971523496967</h1>
+            <h1 className='text-[#340110]'>Call Us</h1>
+            <h1 className='font-semibold text-[#340110]text-[#340110] font-bold '>971523496967</h1>
           </div>
           <div className="dropdown">
             <label onClick={() => { setDrop(!drop); setNavbar(!navbar) }} tabIndex={0} className="">
@@ -90,7 +90,7 @@ const Header = () => {
           
             {
               drop && (
-                <ul onClick={() => setDrop(!drop)} tabIndex={0} className="flex menu menu-compact -right-10 md:-right-24 dropdown-content mt-10 md:mt-12 pb-5 shadow bg-gradient-to-r from-[#f5f0ef] via-[#f5f0ef] to-[#f5f0ef] w-60">
+                <ul onClick={() => setDrop(!drop)} tabIndex={0} className="flex menu menu-compact -right-10 md:-right-24 dropdown-content mt-10 md:mt-14 pb-5 shadow bg-gradient-to-r from-[#f5f0ef] via-[#f5f0ef] to-[#f5f0ef] w-60">
                 {navitem}
               </ul>
                
