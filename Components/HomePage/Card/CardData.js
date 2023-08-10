cdf0fde6d59e593263c1fd7d894e1da7a241be5a
unknown <liptonbarua274@gmail.com>
2023-08-10 21:22:49 +0600

    project

--- Components/HomePage/Card/CardData.js
+++ Components/HomePage/Card/CardData.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 const CardData = ({card}) => {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      offset: 200, 
+      easing: 'ease-in-out',
+    });
+  }, []);
     return (
-        <div className="w-full md:w-72" data-aos="fade-right">
+        <div data-aos="fade-right" className="w-full md:w-72" data-aos="fade-right">
         <figure><img className='h-60 w-full' src={card.img} alt="Shoes" /></figure>
         <div className="">
           <h2 className="card-title text-3xl text-white font-bold">{card.title}</h2>
